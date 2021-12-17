62ccc9bd17db641b62547094b14b5fab02f3bcdc
Greg Miller <hello@gregmiller.io>
2021-12-16 20:49:47 -0500

    feat: Modify base file with nav suggestions

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import ModeSwitcher from './ModeSwitcher.svelte';
 	import Tailwindcss from './Tailwindcss.svelte';
+	import HeroCardBackground from "./HeroCardBackground.svelte";
+	import Navigation from "./Navigation.svelte";
 	export let name: string;
 </script>
 <style>
@@ -10,11 +12,8 @@
 </style>
 <Tailwindcss />
 <ModeSwitcher />
-<main class="p-4 mx-auto text-center max-w-xl">
-	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Hello {name}!</h1>
-	<p class="custom-style mt-[3rem]">
-		Visit the
-		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
-		to learn how to build Svelte apps.
-	</p>
+
+<main class="p-4 mx-auto text-center max-w-full">
+	<Navigation />
+	<HeroCardBackground />
 </main>
