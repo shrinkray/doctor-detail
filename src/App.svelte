bdb69cbb72ba4318b58b5017eb13f835be240802
Greg Miller <hello@gregmiller.io>
2021-12-16 21:51:07 -0500

    feat: Add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
-	import ModeSwitcher from './ModeSwitcher.svelte';
+	// import ModeSwitcher from './ModeSwitcher.svelte';
 	import Tailwindcss from './Tailwindcss.svelte';
 	import HeroCardBackground from "./HeroCardBackground.svelte";
 	import Navigation from "./Navigation.svelte";
 	import Team from "./Team.svelte";
+	import Footer from "./Footer.svelte";
 </script>
 
 <Tailwindcss />
-<ModeSwitcher />
+<!--<ModeSwitcher />-->
 
 <main class="p-4 mx-auto text-center max-w-full">
 	<Navigation />
 	<HeroCardBackground />
 	<Team />
 </main>
+<Footer />
