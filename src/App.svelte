abcefc2d530742df343c3ef961d1a89dc932caf3
Greg Miller <hello@gregmiller.io>
2021-12-16 21:45:39 -0500

    feat: Add a team row
    * Use new dummy images from unsplash
    * Set Eric and Alex names

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,14 @@
 	import Tailwindcss from './Tailwindcss.svelte';
 	import HeroCardBackground from "./HeroCardBackground.svelte";
 	import Navigation from "./Navigation.svelte";
-	export let name: string;
+	import Team from "./Team.svelte";
 </script>
-<style>
-	.custom-style {
-		@apply italic;
-	}
-</style>
+
 <Tailwindcss />
 <ModeSwitcher />
 
 <main class="p-4 mx-auto text-center max-w-full">
 	<Navigation />
 	<HeroCardBackground />
+	<Team />
 </main>
